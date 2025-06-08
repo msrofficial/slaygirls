12da197372b4d197127fa31a8f23e6b754090146
MD Sakibur Rahman <103588995+msrofficial@users.noreply.github.com>
2025-06-08 06:10:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -177,7 +177,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const oldReels = (direction === 'down') ? document.querySelectorAll('.reel.prev') : document.querySelectorAll('.reel.next');
             oldReels.forEach(r => { if (r !== activeReel) r.remove(); });
             isScrolling = false;
-        }, 0);
+        }, 600);
     }
     function playActiveVideo() {
         if (!activeReel) return;
